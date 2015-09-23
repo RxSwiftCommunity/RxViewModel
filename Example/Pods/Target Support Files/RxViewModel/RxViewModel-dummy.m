9e338bb0bcb7a17d353ab92a7556591e9a8c87e9
Esteban Torres <me@estebantorr.es>
2015-09-22 19:28:35 -0600

    Added the Pods to enable Carthage support.

---
+++ Example/Pods/Target Support Files/RxViewModel/RxViewModel-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RxViewModel : NSObject
+@end
+@implementation PodsDummy_RxViewModel
+@end
---
+++ Example/Pods/Target Support Files/RxViewModel/RxViewModel-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double RxViewModelVersionNumber;
+FOUNDATION_EXPORT const unsigned char RxViewModelVersionString[];
+
